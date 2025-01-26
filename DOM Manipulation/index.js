688d99a133c2736a9a81d8f0ec2a2e0b0649b019
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2025-01-26 14:58:48 -0800

    update

---
+++ DOM Manipulation/index.js
@@ -0,0 +1,17 @@
+// Get element by its id
+idEle = document.getElementById('myid')
+
+// Get element by its class
+classEle = document.getElementsByClassName('myclass')
+
+// Get element by its tag
+tagEle = document.getElementsByTagName('h1')
+
+// Get element by its query selector ('.class', '#id', 'tag')
+queryEle = document.querySelector('h3')
+
+// Get element by its query selector all ('.class', '#id', 'tag')
+queryEles = document.querySelectorAll('div')
+
+const headingOne = document.getElementById('myid')
+headingOne.style.color = 'red'
